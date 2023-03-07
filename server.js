be630fe5ac1afc640f9b0414e273af625d3c6e5d
istarN <demirbileknihat@istararastirma.com.tr>
2023-03-07 12:32:39 +0300

    added someting

--- server.js
+++ server.js
@@ -27,7 +27,9 @@ const connect = async () => {
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
-
+app.get("/", (req, res) => {
+  return res.send("fiverr api");
+});
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/gigs", gigRoute);
